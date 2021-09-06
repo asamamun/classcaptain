8f2216bc853dd7f0b45400fc09479561b12a9353
asamamun <asamamun.web@gmail.com>
2021-09-06 12:43:18 +0600

    line added in test.txt

--- index.php
+++ index.php
@@ -28,9 +28,12 @@ $dom->save('browser.xml');
 }
 }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style type="text/css">
 body{font-family:"Trebuchet MS",verdana;width:350px;}
 ul{list-style:none;}
